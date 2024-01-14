a08a60c4ad6245ba2a91483e3f29b482326ddee0
Jonah Köllner <koellnjo@gmail.com>
2024-01-14 11:46:37 +0000

    added logic for adding zone to exsiting ones

--- workspace/prealloc/malloc.c
+++ workspace/prealloc/malloc.c
@@ -15,6 +15,7 @@ size_t zone_calc(size_t size) {
 	return (size);
 }
 
+// Creates and returns a zone. Returns NULL if there was a error
 void *_createZone(size_t size) {
 	t_linkStorage *newZone;
 
@@ -32,6 +33,7 @@ void *_createZone(size_t size) {
 	return (newZone);
 }
 
+// Creates and returns a block. Returns NULL if there was a error
 void *_createBlock(size_t size, void *ptr) {
 	t_linkStorage *newBlock;
 
@@ -44,9 +46,15 @@ void *_createBlock(size_t size, void *ptr) {
 	return (newBlock);
 }
 
+// Special free function only for freeing Zones
 void _freeZone(t_linkStorage *zone) {
 	munmap(zone->ptr, zone->size); // free created zone if there was a error
 	munmap(zone, sizeof(t_linkStorage)); // free created zone if there was a error
+}
+
+// Special free function only for freeing Blocks
+void _freeBlock(t_linkStorage *block) {
+	munmap(block, sizeof(t_linkStorage)); // free created zone if there was a error
 }
 
 // version to save [size][allocatedmemory] with Preallocation to minize system calls and enhance performance
@@ -56,15 +64,20 @@ void *malloc(size_t size) {
 
 	// There are no available zones , create one
 	size_t zone_size = zone_calc(size);
+	t_linkStorage *newZone = _createZone(zone_size);
+
+	if (newZone == NULL) {
+		return (NULL);
+	}
+	if ((newZone->blocks = _createBlock(size, newZone->ptr)) == NULL) { // since its a new zone we can be sure that the first block has the starting point on the beginning of the zone
+		_freeZone(newZone);
+		return (NULL);
+	}
 	if (g_zones == NULL) {
-		// No zone / allocation before
-		if ((g_zones = _createZone(zone_size)) == NULL) {
-			return (NULL);
-		}
-		if ((g_zones->blocks = _createBlock(size, g_zones->ptr)) == NULL) { // since its a new zone we can be sure that the first block has the starting point on the beginning of the zone
-			_freeZone(g_zones);
-			return (NULL);
-		}
+		g_zones = newZone;
+	} else {
+		// There are zones but there is no place left in said zones
+		ft_lstadd_back(&g_zones, newZone);
 	}
 
 	// TODO: This is the "old" version. Remove or encorporate into the new version
@@ -85,4 +98,3 @@ void free(void *ptr) {
 	printf("freeing %p with size %ld\n", realptr, *realptr);
 	munmap(realptr, *realptr + sizeof(size_t));
 }
-
